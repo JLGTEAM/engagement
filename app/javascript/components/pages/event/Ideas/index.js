8524d0426fd407d686ac808e5b9e323d3894a56f
loicboset <loic.boset@gmail.com>
2021-02-06 15:43:11 +0100

    postin ideas

--- app/javascript/components/pages/event/Ideas/index.js
+++ app/javascript/components/pages/event/Ideas/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import { FormControl, InputLabel, Input, Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -8,6 +8,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Checkbox from '@material-ui/core/Checkbox';
 import Avatar from '@material-ui/core/Avatar';
+import { postWithAxios } from '../../../../utils/useAxios';
 
 const Ideas = () => {
 
@@ -20,58 +21,82 @@ const Ideas = () => {
   }));
 
   const classes = useStyles();
-  const [checked, setChecked] = React.useState([1]);
+  const [checked, setChecked] = useState([1]);
+  const [formTitle, setFormTitle] = useState('Submit your idea');
+  const [idea, setIdea] = useState({ title: '', description: '' });
 
   const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
+    currentIndex === -1
+      ? newChecked.push(value)
+      : newChecked.splice(currentIndex, 1)
 
     setChecked(newChecked);
   };
 
-  return(
-    <div className="flex">
-      <List dense className={classes.root}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-secondary-label-${value}`;
-        return (
-          <ListItem key={value} button>
-            <ListItemAvatar>
-              <Avatar
-                alt={`Avatar n°${value + 1}`}
-                src={`/static/images/avatar/${value + 1}.jpg`}
-              />
-            </ListItemAvatar>
-            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            <ListItemSecondaryAction>
-              <Checkbox
-                edge="end"
-                onChange={handleToggle(value)}
-                checked={checked.indexOf(value) !== -1}
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            </ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
-    </List>
+  const handleChangeIdea = e => {
+    const target = e.target;
+    const property = target.id;
+    const value = target.value;
+    setIdea({ ...idea, [property]: value });
+  };
 
-      <form className="flex-column border rounded-xl p-3">
-        <FormControl>
+  const handleSubmitIdea = () => {
+    console.log('idea', idea);
+    postWithAxios('/api/v1/organizations/1/events/1/activities', {
+      title: idea.title,
+      description: idea.description,
+      event_id: 1,
+    })
+    .then(response => {
+      console.log('response', response);
+      setIdea({ title: '', description: '' });
+      setFormTitle('Your idea has been submitted!');
+    })
+  };
+
+  return(
+    <div className="flex justify-between">
+      <List dense className={classes.root}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-secondary-label-${value}`;
+          return (
+            <ListItem key={value} button>
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value + 1}`}
+                  src={`/static/images/avatar/${value + 1}.jpg`}
+                />
+              </ListItemAvatar>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              <ListItemSecondaryAction>
+                <Checkbox
+                  edge="end"
+                  onChange={handleToggle(value)}
+                  checked={checked.indexOf(value) !== -1}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+
+      <form className="flex flex-column flex-grow-1 border rounded-xl p-3 mx-10">
+        <Typography className='pb-2' variant="h6">
+          {formTitle}
+        </Typography>
+        <FormControl className='w-50'>
           <InputLabel htmlFor="my-input">Title</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
+          <Input value={idea.title} onChange={handleChangeIdea} id="title" />
         </FormControl>
-        <FormControl>
+        <FormControl className='w-50'>
           <InputLabel htmlFor="my-input">Description</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
+          <Input value={idea.description} onChange={handleChangeIdea} id="description" />
         </FormControl>
-        <Button variant="contained" color="primary">
+        <Button className='w-25 mt-4' variant="contained" color="primary" onClick={handleSubmitIdea}>
           Submit
         </Button>
       </form>
