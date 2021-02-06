78d934e05a683c1952008df4ab6bffc1dda9fc0d
loicboset <loic.boset@gmail.com>
2021-02-06 09:34:00 +0100

    Model validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+
+  validates :ideation_start_date, presence: true
+  validates :ideation_end_date, presence: true
+  validates :voting_start_date, presence: true
+  validates :voting_end_date, presence: true
+  validates :guessing_start_date, presence: true
+  validates :guessing_end_date, presence: true
+  validates :location, presence: true
+  validates :cost, presence: true
+  validates :event_date, presence: true
 end
